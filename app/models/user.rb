a56403b9460229bc0cb001e3cea2824925cf6226
seijunhou <hojun0223@icloud.com>
2021-03-24 18:04:39 +0900

    直し1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   validates :user_name, presence: true
   validates :profile, presence: true
   validates :occupation, presence: true
+  validates :position, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
